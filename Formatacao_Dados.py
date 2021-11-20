27ef7d51a1ffcf94ce7308537fe8683be01b4b62
Gabriel <40703284+gabrielcte@users.noreply.github.com>
2021-11-20 12:10:05 -0300

    Update Formatacao_Dados.py

--- Formatacao_Dados.py
+++ Formatacao_Dados.py
@@ -1,19 +1,27 @@
+'''
+Gabriel Alves Silva
+https://github.com/gabrielcte/Estudo_Python/blob/main/Formatacao_Dados.py
+Infelizmente nem sempre os dados chegam prontos para o trabalho, assim
+é importante saber formatá-los para o devido uso. Abaixo segue alguns exercícios 
+que fiz para formatação de dados.
+'''
+
+# Rotina
 empresa = 'Google'
 print(empresa)
 
 empresa = "Google"
 print(empresa)
 
-empresa = "Let's Code"
-print(empresa)
-
-frase = "O professor Pietro da Let's Code disse: \"Hoje a pizza é por minha conta\""
-print(frase)
-
 empresa = 'Google'
 print(empresa[0])
 print(empresa[:3])
 
+empresa = "Let's Go"
+print(empresa)
+
+frase = "O professor Gabriel da Let's Go disse: \"Hoje a pizza é por minha conta\""
+print(frase)
 
 nomes_cidades = "São Paulo, Belo Horizonte, Rio de Janeiro, Brasília"
 nomes_cidades = nomes_cidades.split(', ')
@@ -34,8 +42,8 @@ while nome_cidade.lower() != 'rio de janeiro':
     print('Tenta de novo, vai')
     nome_cidade = input('Que cidade do Brasil é conhecida como cidade maravilhosa?')
 
-print('Boooa, campeão!')
+print('Muito Bem!')
 
-mensagem = 'Você viu o que o Pietro disse na sala ontem?'
-fui_citado = 'Pietro' in mensagem
+mensagem = 'Você viu o que o Gabriel disse na sala ontem?'
+fui_citado = 'Gabriel' in mensagem
 print(fui_citado)
