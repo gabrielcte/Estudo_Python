29904cccc95093e5642725c51ea9663917e8f08f
Gabriel <40703284+gabrielcte@users.noreply.github.com>
2021-11-20 13:01:19 -0300

    Update Tipos_Variaveis.py

--- Tipos_Variaveis.py
+++ Tipos_Variaveis.py
@@ -1,3 +1,11 @@
+'''
+Gabriel Alves Silva
+https://github.com/gabrielcte/Estudo_Python/blob/main/Analise_Tipos_Variaveis.py
+Os dados podem ter varios tipos, e cada tipo tem suas operações e funções, é importante identificá-los.
+Segue o exemplo:
+'''
+
+# Rotina
 x = 5
 print(x)
 print(type(x))
