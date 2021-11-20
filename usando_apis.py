1be52d5c5d062f2dc02a93f3c6e4e463783e9bd5
Gabriel <40703284+gabrielcte@users.noreply.github.com>
2021-11-20 13:13:45 -0300

    Update usando_apis.py

--- usando_apis.py
+++ usando_apis.py
@@ -1,4 +1,15 @@
+'''
+Gabriel Alves Silva
+https://github.com/gabrielcte/Estudo_Python/blob/main/usando_apis.py
+As APIs são composta por uma série de funções acessíveis somente por programação,
+e que permitem utilizar características desse software. Nesse exemplo foi usado uma API publica Web
+que tem um conjunto definido de mensagens de requisição e resposta HTTP,expresso no formatos JSON.
+'''
+
+# Importando bibliotecas
 import requests
+
+# Rotina
 url = 'https://api.exchangerate-api.com/v6/latest'
 req = requests.get(url)
 print(req.status_code)
